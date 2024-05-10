bc3271112e7979a589fc933018dd4a9823567bbb
chakradhar-brs <ideal.chakri@gmail.com>
2024-05-09 22:33:46 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AnalogClock from './AnalogClock';
+import './BG.png'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Analog Clock</h1> 
+      <AnalogClock />  
+      
+    </div>
+  );
+}
+
+export default App;
